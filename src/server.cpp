c79a744e8d784bd8c1d7999508a0584eec1127a3
chitrak-singhal <chitrak@DESKTOP-SBRPPMD.localdomain>
2024-06-01 04:44:28 +0530

    Reading header

--- src/server.cpp
+++ src/server.cpp
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
           break;
         }
       }
-      response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(text.size()) + "\r\n" + text;
+      response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(text.size()) + "\r\n\r\n" + text;
   }
   else
   {
