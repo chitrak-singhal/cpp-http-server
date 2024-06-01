999a19e3e3c9c3ed5065290bff1c3bc8382b17d6
chitrak-singhal <chitrak@DESKTOP-SBRPPMD.localdomain>
2024-06-01 14:54:46 +0530

    Refactored code

---
+++ src/client_handler.h
@@ -0,0 +1,20 @@
+#ifndef CLIENT_HANDLER_H
+#define CLIENT_HANDLER_H
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include<bits/stdc++.h>
+#include <thread>
+using namespace std;
+
+vector<string> split(string &request, string delim);
+void handleClient(int client, string directory);
+
+#endif
--- src/server.cpp
+++ src/server.cpp
@@ -9,81 +9,9 @@
 #include <netdb.h>
 #include<bits/stdc++.h>
 #include <thread>
+#include "client_handler.h"
 using namespace std;
 
-vector<string> split(string &request, string delim)
-{
-    stringstream ss (request);
-    vector<string> comps;
-    string temp;
-    while(getline(ss, temp, delim[0]))
-    {
-      comps.push_back(temp);
-      ss.ignore(delim.size()-1);
-    }
-    return comps;
-}
-
-void handleClient(int client, string directory)
-{
-  char buffer[4096];
-  int bytes_received = recv(client, buffer, sizeof(buffer) - 1, 0);
-  buffer[bytes_received] = '\0'; 
-  string request(buffer);
-
-  vector<string> comps = split(request, "\r\n");
-  vector<string> comps2 = split(comps[0], " ");
-  string method = comps2[0];
-  string endpoint = comps2[1];
-  string response="";
-  if (endpoint=="/")
-      response = "HTTP/1.1 200 OK\r\n\r\n";
-  else if (endpoint.starts_with("/echo"))
-  {
-      string text= endpoint.substr(6);
-      response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(text.size()) + "\r\n\r\n" + text;
-      
-  }
-  else if (endpoint.starts_with("/user"))
-  {
-    string text;
-      for (auto &elem:comps)
-      {
-        if (elem.starts_with("User"))
-        {
-          vector<string> temp = split(elem, " ");
-          text = temp[1];
-          break;
-        }
-      }
-      response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(text.size()) + "\r\n\r\n" + text;
-  }
-  else if (endpoint.starts_with("/files"))
-  {
-    string filepath = directory+endpoint.substr(6);
-    ifstream file(filepath);
-    if (file)
-    {
-        stringstream content;
-        content << file.rdbuf();
-        file.close();
-        string text = content.str();
-        response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + to_string(text.size()) + "\r\n\r\n" + text;
-    }
-    else
-    {
-        response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found";
-    }
-  }
-  else
-  {
-      response = "HTTP/1.1 404 Not Found\r\n\r\n";
-  }
-
-  send(client, response.c_str(), response.length(), 0);
-  cout << "Client connected\n";
-  close(client);
-}
 
 int main(int argc, char **argv)
 {
